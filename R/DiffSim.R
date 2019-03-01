9c7f0f0c3c5ee29a3d0763d6a1642194475bd1a6
whopper-cemnl <wjhopper510@gmail.com>
2019-03-01 16:31:22 -0500

    Simulation implementation of the diffusion model with an SDT criterion check bolted on.

---
+++ R/DiffSim.R
@@ -0,0 +1,48 @@
+diffusion_SDT_sim <- function(N, a, v, t0, z, sv, st0, sz=0, s=1, crit_old=-0.5, crit_new=0.5) {
+
+  z <- z*a # convert relative starting point to absolute
+  dt <- .001 # time step size
+
+  sim_data <- matrix(0, nrow = N, ncol=3,
+                     dimnames = list(NULL, c("RT","speeded_resp","delayed_resp"))
+                     )
+
+  NDT <- runif(1, t0 - st0/2, t0 + st0/2) # non-decision_times
+  evidence <- rnorm(N, v, sv) # Sample SDT evidence strengths / drift rates
+  drifts <- evidence* dt # Sample sampled evidence scale to instantaneous drift
+  s <- sqrt(s^2 * dt) # scale drift coefficient to instantaneous s.d.
+
+  if (sz > 0) {
+    start_points <- runif(N, z-.5*sz, z+.5*sz)
+  } else {
+    start_points <- rep(z, N)
+  }
+
+  for (i in 1L:N){
+
+    step <- 0
+    pos <- start_points[i]
+    v_inst <- drifts[i]
+
+    while(!(pos > a | pos < 0 )) {
+      step <- step + 1
+      pos <- pos + rnorm(1, v_inst, s)
+    }
+
+    if (pos >= a){
+      sim_data[i, "speeded_resp"] <- 1
+    }
+
+    sim_data[i, "RT"] <- step # Record number of steps
+  }
+
+  sim_data[, "RT"] <- NDT + sim_data[, "RT"]*dt # convert steps to RT
+
+  above_crit_o <- evidence > crit_old
+  above_crit_n <- evidence > crit_new
+  said_old <- sim_data[i, "speeded_resp"] == 1
+  sim_data[above_crit_o & said_old, "delayed_resp"] <- 1
+  sim_data[above_crit_n & !said_old, "delayed_resp"] <- 1
+
+  return(sim_data)
+}
