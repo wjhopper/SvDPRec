9731c52b1fbffc3ac9569f6ef9769b874fbc2fbc
whopper <wjhopper510@gmail.com>
2019-05-28 18:51:50 -0400

    sample starting points and non-decisions times in parallel.

--- src/diffusion_parallel.cpp
+++ src/diffusion_parallel.cpp
@@ -11,41 +11,50 @@ using namespace RcppParallel;
 struct Diffusion : public Worker
 {   
   RMatrix<double> sim_data;
-  RVector<double> NDT;
-  RVector<double> start_points;
   double a;
   double v;
+  double t0;
+  double z;
   double sv;
+  double sz;
+  double st0;
   double s;
   RVector<double> crit;
   double dt;
-  dqrng::normal_distribution rnorm;
-  
+
   // constructor
   Diffusion(Rcpp::NumericMatrix sim_data,
-            Rcpp::NumericVector NDT,
-            Rcpp::NumericVector start_points,
-            double a, double v, double sv, double s,
+            double a, double v, double t0, double z, double sv, double sz, double st0, double s,
             Rcpp::NumericVector crit, double dt
             ) :
-    sim_data(sim_data), NDT(NDT), start_points(start_points), a(a), v(v), sv(sv), s(s), crit(crit), dt(dt)
+    sim_data(sim_data), a(a), v(v), t0(t0), z(z), sv(sv), sz(sz), st0(st0), s(s), crit(crit), dt(dt)
     {}
   
   void operator()(std::size_t begin, std::size_t end) {
     pcg64 rng(42, end);
-
+    
+    dqrng::normal_distribution evidence_dist(v, sv);
+    dqrng::normal_distribution noise(0, s);
+    dqrng::uniform_distribution NDT(t0, t0 + st0);
+    dqrng::uniform_distribution start_points(z-.5*sz, z+.5*sz);
+    
     for(std::size_t i = begin; i < end; i++) {
-      double evidence = v + rnorm(rng)*sv;  // Sample SDT evidence strength \ drift rate
+      double evidence = evidence_dist(rng);  // Sample SDT evidence strength \ drift rate
       double drift = evidence*dt; // Sample sampled evidence scale to instantaneous drift
       
-      int step = 0;
-      double pos = start_points[i];
+      double pos;
+      if (sz > 0) {
+        pos = start_points(rng);
+      } else{
+        pos = z;
+      }
       
+      int step = 0;
       while(pos < a && pos > 0 ) {
-        pos += drift + rnorm(rng)*s;
+        pos += drift + noise(rng);
         step += 1;
       }
-      sim_data(i, 0) = NDT[i] + step*dt; // column 1 = rt
+      sim_data(i, 0) = NDT(rng) + step*dt; // column 1 = rt
       
       if (pos >= a) {
         sim_data(i, 1) = 1; // column 2 = speeded response
@@ -72,21 +81,13 @@ Rcpp::NumericVector diffusion_parallel(int N, double a, double v, double t0,
   
   double dt = .001; // timestep size
   s = sqrt(pow(s, 2.0l) * dt); // scale drift coefficient to instantaneous s.d.
+  z = z * a; // convert relative starting point to absolute
   
   // allocate the output vector
   NumericMatrix sim_data(N, 3);
   colnames(sim_data) = CharacterVector::create("RT", "speeded_resp","delayed_resp");
-  z = z * a; // convert relative starting point to absolute
-  
-  NumericVector NDT = runif(N, t0, t0 + st0); // non-decision_times
-  NumericVector start_points;
-  if (sz > 0) {
-    start_points = runif(N, z-.5*sz, z+.5*sz);
-  } else {
-    // NumericVector z_vector = NumericVector::create(z);
-    start_points = rep_len(NumericVector::create(z), N);
-  }
-  Diffusion diffusion(sim_data, NDT, start_points, a, v, sv, s, crit, dt);
+
+  Diffusion diffusion(sim_data, a, v, t0, z, sz, sv, st0, s, crit, dt);
   RcppParallel::parallelFor(0, N, diffusion);
   return sim_data;
 }
